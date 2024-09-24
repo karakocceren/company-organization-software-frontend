1bc99fb4df54c72f739e9083b590b343e97fb471
Ceren Karakoç <02cerenkarakoc@gmail.com>
2024-09-24 09:40:57 +0300

    Changed backend url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "http://company-organization.eu-central-1.elasticbeanstalk.com",
   headers: {
     "Content-Type": "application/json",
   },
