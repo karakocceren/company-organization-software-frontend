5e983ade16cc992433e5380485ff9426af5bc311
Ceren Karakoç <02cerenkarakoc@gmail.com>
2024-09-26 10:09:57 +0300

    Implemented profile picture update

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://company-organization.eu-central-1.elasticbeanstalk.com",
+  baseURL:
+    "https://delta-company-organization.eu-central-1.elasticbeanstalk.com",
   headers: {
     "Content-Type": "application/json",
   },
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,11 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useTranslation } from "react-i18next";
-import { Typography, Box, Avatar, Chip } from "@mui/material";
+import { Typography, Box, Avatar, Chip, Button } from "@mui/material";
+import ErrorMessage from "./ErrorMessage";
 import useAuth from "../hooks/useAuth";
+import axios from "../api/axios";
+
+const GET_PROFILE_PICTURE_URL = "/api/v1/profile/profile-picture";
+const UPLOAD_PROFILE_PICTURE_URL = "/api/v1/profile/upload-profile-picture";
 
 const Profile = () => {
   const { t } = useTranslation();
   const { auth } = useAuth();
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [profilePicture, setProfilePicture] = useState(null);
+
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+  const [snackbarMessage, setSnackbarMessage] = useState("");
+  const [snackbarSeverity, setSnackbarSeverity] = useState("error");
+
+  useEffect(() => {
+    fetchProfilePicture();
+  }, []);
+
+  const fetchProfilePicture = async () => {
+    try {
+      const response = await axios.get(GET_PROFILE_PICTURE_URL, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${
+            JSON.parse(localStorage.getItem("auth")).token
+          }`,
+        },
+      });
+      setProfilePicture(response.data);
+    } catch (error) {
+      console.error("Error fetching profile picture");
+    }
+  };
 
   const getRoleInfo = (role) => {
     switch (role) {
@@ -17,6 +48,44 @@ const Profile = () => {
         return { role: t("role_employee"), color: "primary" };
       default:
         return "primary";
+    }
+  };
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+
+    // Check if the selected file is an image
+    if (file && !file.type.startsWith("image/")) {
+      alert("Please select a valid image file (JPG, PNG, etc.)");
+      setSelectedFile(null); // Clear selection if invalid file
+      return;
+    }
+
+    setSelectedFile(file); // Store valid file
+  };
+
+  const handleUpload = async () => {
+    if (!selectedFile) return;
+
+    const formData = new FormData();
+    formData.append("file", selectedFile);
+
+    try {
+      const response = await axios.post(UPLOAD_PROFILE_PICTURE_URL, formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+          Authorization: `Bearer ${
+            JSON.parse(localStorage.getItem("auth")).token
+          }`,
+        },
+      });
+
+      if (response.status === 200) {
+        const newProfilePictureUrl = URL.createObjectURL(selectedFile);
+        setProfilePicture(newProfilePictureUrl);
+      }
+    } catch (error) {
+      console.error("Failed to upload the profile picture", error);
     }
   };
 
@@ -35,13 +104,37 @@ const Profile = () => {
       <Box
         sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}
       >
-        <Avatar sx={{ width: 150, height: 150 }}>U</Avatar>
+        <Avatar
+          sx={{ width: 150, height: 150 }}
+          src={profilePicture || undefined}
+          alt="Profile Picture"
+        >
+          {profilePicture ? null : "U"}
+        </Avatar>
+
         <Chip
           label={auth?.role ? roleName : "USER"}
           color={roleColor}
           size="small"
           sx={{ maxWidth: "100px", marginTop: "10px" }}
         />
+        <Button variant="text" component="label" sx={{ padding: "5px 0" }}>
+          upload_picture
+          <input
+            type="file"
+            hidden
+            accept="image/*"
+            onChange={handleFileChange}
+          />
+        </Button>
+        <Button
+          variant="text"
+          onClick={handleUpload}
+          disabled={!selectedFile}
+          sx={{ padding: "0" }}
+        >
+          submit_picture
+        </Button>
       </Box>
       <Box
         sx={{
